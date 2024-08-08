759d2894acabe4c8bb80c6e166cec4326bdf4a73
Spoorti2001 <spoortikoshavar@gmail.com>
2024-08-08 14:23:57 +0530

    changes

--- Instant-Meetings/src/main/java/com/example/demo/User/UserController.java
+++ Instant-Meetings/src/main/java/com/example/demo/User/UserController.java
@@ -22,6 +22,11 @@ public class UserController {
 
 	 private final UserService service;
 
+	  @GetMapping("/")
+	    public String hello() {
+	        return "Hello, World!";
+	    }
+	 
 	    // Manually defined constructor for dependency injection
 	    public UserController(UserService service) {
 	        this.service = service;
